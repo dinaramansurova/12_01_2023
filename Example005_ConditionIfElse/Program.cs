4e74e07ae6871418bb5b870ace8406397f85aa64
Dinara <ash.solarium@gmail.com>
2023-01-12 22:36:09 +0300

    add Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "динара")
+{
+    Console.WriteLine("Ура, это же Динара!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
