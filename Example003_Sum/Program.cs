6fba6ef6494d77883d1ef262eb60ab05d6b9c243
Dinara <ash.solarium@gmail.com>
2023-01-12 18:49:40 +0300

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 22;
-int numberB = 19;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
